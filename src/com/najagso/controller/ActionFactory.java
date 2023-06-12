83a782a0f5f77a9c8c68362b1fb626e0f7afa94d
Hyunfor <navel5148@gmail.com>
2023-06-12 23:22:49 +0900

    Action, ActionFactory, join 기능 추가 작업 중

--- src/com/najagso/controller/ActionFactory.java
+++ src/com/najagso/controller/ActionFactory.java
@@ -1,6 +1,7 @@
 package com.najagso.controller;
 
 import com.najagso.controller.action.Action;
+import com.najagso.controller.action.JoinAction;
 import com.najagso.controller.action.LoginAction;
 import com.najagso.controller.action.LoginFormAction;
 import com.najagso.controller.action.LogoutAction;
@@ -20,13 +21,17 @@ public class ActionFactory {
 		Action action = null;
 		System.out.println("ActionFactory  :" + command);
 		/* 일반 유저 */
+		
 		if (command.equals("login_form")) {
 			action = new LoginFormAction();
 		} else if (command.equals("login")) {
 			action = new LoginAction();
 		} else if (command.equals("logout")) {
 			action = new LogoutAction();
+		} else if (command.equals("join")) {
+			action = new JoinAction(); 
 		}
+		
 		return action; 
 	}
 }
